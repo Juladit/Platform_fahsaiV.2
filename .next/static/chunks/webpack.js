4cef27ed570bdf8b45c97a943aa37f296d0ba9d6
Juladit <157340273+Juladit@users.noreply.github.com>
2025-11-02 14:35:05 +0700

    Login_page Finished

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1beff7404f4dfcef"; }
+/******/ 		__webpack_require__.h = function() { return "d0650bf1a87233ae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
   return (
     <header className="w-full max-w-full">
       {/* Keep header background white so it stands out over the page bg */}
-      <Link href="/" className="flex items-center gap-4 no-underline bg-white rounded-md px-3 py-4 shadow-sm">
+      <Link href="/" className="flex items-center gap-2 no-underline bg-white rounded-md px-3 py-4 shadow-sm">
         <div className="relative w-10 h-10 rounded-full overflow-hidden">
           {/* Place your logo file at /public/MFU_logo.png */}
-          <Image src="/logo-mfu-v2.png" alt="MFU logo" fill className="object-contain" />
+          <img src="/img/logo-mfu-v2.png" alt="MFU logo" className="w-20 h-10 object-contain" />
         </div>
 
         <div className="text-lg font-semibold">
